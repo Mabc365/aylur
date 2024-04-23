04a4883666a95e0ddbc6bc46fe274c0fe7d6cc83
Mabc365 <85178607+Mabc365@users.noreply.github.com>
2024-04-22 23:47:20 -0400

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,5 @@
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. ~/.nix-profile/etc/profile.d/nix.sh
+export NIX_CONFIG="experimental-features = nix-command flakes"
+
+nix profile install github:Aylur/dotfiles
